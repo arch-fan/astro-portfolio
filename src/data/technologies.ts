bdb6630da8c6a41cea7797cf26c55fff04843cd8
arch-fan <juanserracines@gmail.com>
2024-04-14 11:37:28 +0200

    Techs updated

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,58 +1,79 @@
 export interface Technology {
-    tech: string;
-    icon: string;
-    percentage: number;
+  tech: string;
+  icon: string;
 }
 
 export const technologies: Technology[] = [
-    {
-        tech: "Python",
-        icon: "akar-icons:python-fill",
-        percentage: 50
-    },
-    {
-        tech: "TailwindCSS",
-        icon: "mdi:tailwind",
-        percentage: 70
-    },
-    {
-        tech: "JavaScript",
-        icon: "akar-icons:javascript-fill",
-        percentage: 70
-    },
-    {
-        tech: "TypeScript",
-        icon: "simple-icons:typescript",
-        percentage: 70
-    },
-    {
-        tech: "Rust",
-        icon: "simple-icons:rust",
-        percentage: 20
-    },
-    {
-        tech: "NextJS",
-        icon: "akar-icons:nextjs-fill",
-        percentage: 60
-    },
-    {
-        tech: "Astro",
-        icon: "simple-icons:astro",
-        percentage: 85
-    },
-    {
-        tech: "Java",
-        icon: "cib:java",
-        percentage: 50
-    },
-    {
-        tech: "React",
-        icon: "mdi:react",
-        percentage: 60
-    },
-    {
-        tech: "SEO",
-        icon: "icon-park-outline:seo",
-        percentage: 60
-    }
-]
+  {
+    tech: "Python",
+    icon: "mdi:language-python",
+  },
+  {
+    tech: "TailwindCSS",
+    icon: "mdi:tailwind",
+  },
+  {
+    tech: "JavaScript",
+    icon: "mdi:language-javascript",
+  },
+  {
+    tech: "TypeScript",
+    icon: "mdi:language-typescript",
+  },
+  {
+    tech: "Rust",
+    icon: "mdi:language-rust",
+  },
+  {
+    tech: "NextJS",
+    icon: "akar-icons:nextjs-fill",
+  },
+  {
+    tech: "Astro",
+    icon: "simple-icons:astro",
+  },
+  {
+    tech: "Java",
+    icon: "mdi:language-java",
+  },
+  {
+    tech: "React",
+    icon: "mdi:react",
+  },
+  {
+    tech: "SEO",
+    icon: "icon-park-outline:seo",
+  },
+  {
+    tech: "Docker",
+    icon: "mdi:docker",
+  },
+  {
+    tech: "Svelte",
+    icon: "simple-icons:svelte",
+  },
+  {
+    tech: "Node.js",
+    icon: "mdi:nodejs",
+  },
+  {
+    tech: "MongoDB",
+    icon: "simple-icons:mongodb",
+  },
+  {
+    tech: "Linux",
+    icon: "mdi:linux",
+  },
+  {
+    tech: "Git",
+    icon: "mdi:git",
+  },
+  {
+    tech: "Github",
+    icon: "mdi:github",
+  },
+  {
+    tech: "Microsoft SQL Server",
+    icon: "simple-icons:microsoftsqlserver",
+  },
+];
